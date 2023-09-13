2118670fc9ad37f921829188025c5fa38739b941
Joel Luis <joelluis73@gmail.com>
2023-09-12 22:34:46 -0300

    create server ai

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import { fastify } from 'fastify'
+import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAiCompletionRoute } from './routes/generate-ai-completion'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
+import { fastifyCors } from '@fastify/cors'
+
+const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!🚀')
+  })
